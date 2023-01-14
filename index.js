ebb45c8a4e287ee9958bae7d22d18c93fa73a27c
Anshitachoubey07 <92245615+Anshitachoubey07@users.noreply.github.com>
2023-01-14 16:08:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".jpg";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".jpg";
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
+}
+
+else if( randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+}
+
+else{
+    document.querySelector("h1").innerHTML = "Draw!!";
+}
